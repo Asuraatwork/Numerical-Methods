bcd6a22dd9dad288a5f11bcaf57febf3822bc95e
Saikat <saikatspidey@gmail.com>
2021-06-24 00:13:27 +0530

    first commit

---
+++ bisection_method/bisection.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+#define E .000001
+
+double func(double x){
+ return  ((pow(x,3)-x-1));// provide the equation
+}
+double bisection(double a,double b){
+    return (a+b)/2;
+}
+
+int main(){
+int i=0;
+double a,b,xr;
+printf("Enter the values of a and b : " );
+scanf("%lf %lf",&a,&b );
+/****************************************************
+if (func(a)*func(b)>0) {
+  perror("Error:THE root must be in interval");
+  exit(1);
+}****************************************************/
+//xr=bisection(a,b);
+while((b-a)>=E){
+  xr=bisection(a,b);
+
+  printf("a = %lf ,b = %lf xr = %lf || f(xr) = %lf \n",a,b,xr,func(xr));
+
+
+i++;
+if(func(xr)==0.0){
+  break;
+}
+xr=bisection(a,b);
+if (func(xr)*func(a)<0) {
+  b=xr;
+} else {
+  a=xr;
+}
+
+}
+printf("After %d interval : THE root is %lf\n",i,xr  );
+
+}
